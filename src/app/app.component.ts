7dbc47119eac98799a26c6a650f03e7c25a1a444
Marvin Frede <mfrede@slashwhy.de>
2023-08-21 13:44:50 +0200

    setup test client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Apollo, gql } from 'apollo-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-test-apollo-app';
+  public title = 'angular-test-apollo-app';
+  private apollo = inject(Apollo);
+
+  constructor() {
+    this.apollo
+      .query({
+        query: gql`
+          query GetLocations {
+            locations {
+              id
+              name
+              description
+              photo
+            }
+          }
+        `,
+      })
+      .pipe(takeUntilDestroyed())
+      .subscribe((result) => console.log(result));
+  }
 }
